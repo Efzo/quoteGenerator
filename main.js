9851894a4a40e0ad147c449c932ec8a69c0aea27
Efosa Ojomo <76777601+Efzo@users.noreply.github.com>
2023-09-05 03:38:22 -0500

    Random Quote generator

---
+++ main.js
@@ -0,0 +1,38 @@
+
+
+async function quoteGenerator(){
+  try{
+      const quoteText =  await fetch("https://api.quotable.io/random");
+      const data =  await quoteText.json();
+      return data.content;
+  }catch (error){
+      console.log("Failed to fetch quotes because of " + error);
+      return "Kindly check back later";
+  }
+}
+
+async function showQuote(){
+    const showQuoteText = document.getElementById("quote");
+    const quoteData = await quoteGenerator();
+    return showQuoteText.textContent = quoteData;
+}
+
+//To share post to twitter
+function twitterPost(quote){
+    const twiLink = `https://twitter.com/intent/tweet?text=${encodeURIComponent(quote)}`;
+    window.open(twiLink, '_blank');
+}
+
+const tweetButton = document.getElementById("tweeterButton")
+    tweetButton.addEventListener("click", function (){
+    const quote = document.getElementById("quote").textContent;
+    twitterPost(quote);
+});
+
+
+//to generate the button
+const generateButton = document.getElementById("quoteButton");
+
+generateButton.addEventListener("click", showQuote);
+
+quoteGenerator();
